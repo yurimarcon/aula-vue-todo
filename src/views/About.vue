de2c5fbdaac5c25f947eb0a28ed68c9938eee9c5
fernandohre <senhor.clinton@gmail.com>
2021-07-28 23:42:30 -0300

    :recycle: map do state e mutations

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>
+      <p>
+        A todo list, também conhecida como lista de coisas a fazer, é uma
+        ferramenta extremamente simples, mas que pode mudar completamente a
+        forma com a qual gerencia o seu tempo e fazer com que se torne uma
+        pessoa muito mais produtiva, tanto em relação à vida pessoal quanto
+        profissional
+      </p>
+    </h1>
   </div>
 </template>
